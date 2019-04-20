31ea57a1e1c0504b83ab97ea1a2bbe9f007e6539
Will Glass-Husain <wglass@forio.com>
2019-04-19 17:24:51 -0700

    Remove duplicate fields in BSON that break MongoDb 4.0

--- src/misc.lua
+++ src/misc.lua
@@ -1,3 +1,18 @@
+local skip_duplicate = function ( t, sk, k )
+  local nk, nv
+  if k then
+    nk, nv = next(t, k)
+  else
+    nk, nv = next(t)
+  end
+
+  while nk and nk == sk do
+    nk, nv = next(t, nk)
+  end
+  return nk, nv
+end
+
+-- substitute pairs function to bump up the provided argument to be the first item in the list
 local pairs_start = function ( t , sk )
   local i = 0
   return function(t, k, v)
@@ -6,9 +21,9 @@ local pairs_start = function ( t , sk )
     if i == 1 then
       return sk, t[sk]
     elseif i == 2 then
-      nk, nv = next(t)
+      nk, nv = skip_duplicate( t, sk )
     else
-      nk, nv = next(t, k)
+      nk, nv = skip_duplicate( t, sk, k )
     end
     return nk,nv
   end,
